3a0d2f3a0b969a8d18e3ff54f058250f3b2626a0
Oleg Pavlov <4pavlovon@gmail.com>
2018-10-28 22:52:45 +0400

    Styles + translates

--- templates/products_list.php
+++ templates/products_list.php
@@ -5,7 +5,7 @@
 ?>
 
 <?php if (!empty($products)): ?>
-    <section id="ecwidlatestproducts-3" class="widget widget_ecwidlatestproducts">
+    <section id="bestsellers_for_ecwid" class="widget widget_bestsellers_for_ecwid">
         <!-- noptimize -->
         <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?15362109&amp;data_platform=wporg"></script>
         <script type="text/javascript">
@@ -15,67 +15,20 @@
             }
         </script>
         <!-- /noptimize -->
-        <div class="ecwidlatestproducts ecwid-productsList width-l" data-ecwid-max="3" data-ecwidlatestproducts-initialized="1">
+        <div class="bestsellers_for_ecwid ecwid-bs-productsList width-l" data-ecwid-max="3" data-bestsellers_for_ecwid-initialized="1">
             <ul>
                 <?php foreach($products as $product): ?>
-                    <li class="ecwid-productsList-product-<?php echo $product->id ?> show">
+                    <li class="ecwid-bs-productsList-product-<?php echo $product->id ?> show">
                         <a href="<?php echo $product->link ?>" title="<?php echo $product->name ?>">
-                            <div class="ecwid-productsList-image">
+                            <div class="ecwid-bs-productsList-image">
                                 <img src="<?php echo $product->picture ?>">
                             </div>
-                            <div class="ecwid-productsList-name"><?php echo $product->name ?></div>
-                            <div class="ecwid-productsList-price ecwid-productBrowser-price"><?php echo $product->price ?>р.</div>
+                            <div class="ecwid-bs-productsList-name"><?php echo $product->name ?></div>
+                            <div class="ecwid-bs-productsList-price ecwid-bs-productBrowser-price"><?php echo $product->price ?>р.</div>
                         </a>
                     </li>
                 <?php endforeach; ?>
             </ul>
-        </div>
-        <div class="ecwid-initial-productsList-content">
-            <a class="product" href="<?php echo $product->link ?>"
-               data-ecwid-page="product"
-               data-ecwid-product-id="<?php echo $product->id ?>" alt="<?php echo $product->name ?>"
-               title="Kinder">
-                <div class="ecwid ecwid-SingleProduct ecwid-Product ecwid-Product-<?php echo $product->id ?> loaded"
-                     data-single-product-link="<?php echo $product->link ?>" itemscope=""
-                     itemtype="http://schema.org/Product" data-single-product-id="<?php echo $product->id ?>"
-                     style="font-size: 14px;">
-                    <form>
-                        <div itemprop="image" data-force-image="<?php echo $product->picture ?>">
-                            <div class="ecwid-SingleProduct-picture" style="">
-                                <img class="gwt-Image"
-                                     src="<?php echo $product->picture ?>"
-                                     alt="Kinder" title="<?php echo $product->name ?>"
-                                     itemprop="image"
-                                     srcset="<?php echo $product->picture ?> 1x, <?php echo $product->picture ?> 2x"
-                                     style="width: 270px; height: 270px; margin: auto;"
-                                >
-                            </div>
-                        </div>
-                        <div class="ecwid-title" itemprop="name"><?php echo $product->name ?></div>
-                        <div itemtype="http://schema.org/Offer" itemscope="" itemprop="offers">
-                            <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="<?php echo $product->price ?>">
-                                <div>
-                                    <div><?php echo $product->price ?>р.</div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <!-- noptimize -->
-                <script type="text/javascript">xSingleProduct();</script>
-                <div id="SingleProduct-1" class="ecwid">
-                    <div>
-                        <div></div>
-                    </div>
-                </div><!-- /noptimize -->
-            </a>
-            <script type="text/javascript">
-                <!--
-                jQuery(document).ready(function () {
-                    jQuery('.ecwidlatestproducts:not([data-ecwidlatestproducts-initialized=1])').productsList().attr('data-ecwidlatestproducts-initialized', 1);
-                });
-                -->
-            </script>
         </div>
     </section>
 <?php endif; ?>
